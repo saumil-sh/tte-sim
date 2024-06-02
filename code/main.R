47ee93cad5967425dced7648c91cdf95bf094951
Saumil Shah <samdon97@gmail.com>
2024-06-02 21:01:03 +0200

    added sample size estimation

--- code/main.R
+++ code/main.R
@@ -1,19 +1,24 @@
-library("MASS")
-library("flexsurv")
+#%% import libraries
+library("parallel")
+library("survival")
 library("rstpm2")
 library("coxme")
+library("flexsurv")
 library("simsurv")
 library("ggplot2")
 library("ggpubr")
-#%%
-# Load and examine myeloid data
-data(myeloid, package = "survival")
+library("survminer")
+library("gsDesign")
+
+#%% Load and process myeloid data
 head(myeloid)
 myeloid$trt[myeloid$trt == "A"] <- 0
 myeloid$trt[myeloid$trt == "B"] <- 1
 myeloid$trt <- as.numeric(myeloid$trt)
 
+#%% plot KM overall survival
 fit <- survfit(Surv(futime, death) ~ trt, data = myeloid)
+fit_cox <- coxph(Surv(futime, death) ~ trt, data = myeloid)
 
 ggsurvplot(
   fit,
@@ -50,7 +55,30 @@ plot(mod_flex,
   ylab = "Survival probability",
   xlab = "Time"
 )
-#%%
+#%% sample size est
+trt1_rmst <- summary(fit)$table[9]
+trt2_rmst <- summary(fit)$table[10]
+
+lambda1 <- 1 / trt1_rmst
+lambda2 <- 1 / trt2_rmst
+
+eta <- 1 / summary(survfit(Surv(futime, !death) ~ 1, data = myeloid[myeloid$death == 1, ]))$table[5]
+
+ratio <- exp(fit_cox$coefficients)
+
+SSE <- nSurvival(
+  lambda1 = lambda1,
+  lambda2 = lambda2,
+  Ts = 2500,
+  Tr = 90,
+  eta = eta,
+  ratio = ratio,
+  alpha = 0.025,
+  beta = 0.1,
+  approx = TRUE
+)
+
+#%% custom log cumulative harazard
 
 # Define a function that returns the log cumulative hazard for an FPM model
 logcumhaz_weib <- function(t, x, betas, knots) {
@@ -74,15 +102,15 @@ logcumhaz_flex <- function(t, x, betas, knots) {
 }
 
 #%%
-cov <- data.frame(id = 1:650, trt = rbinom(650, 1, 0.5))
+cov <- data.frame(id = 1:ceiling(SSE$n), trt = rbinom(ceiling(SSE$n), 1, 0.5))
   # Simulate event times using simsurv
 dat_weib <- simsurv(
   betas = mod_weib$coefficients,
   x = cov,
   knots = mod_weib$knots,
   logcumhazard = logcumhaz_weib,
-  maxt = 2500,
-  interval = c(1E-8, 2501)
+  maxt = 3000,
+  interval = c(1E-8, 3001)
 )
 
 dat_flex <- simsurv(
@@ -90,25 +118,33 @@ dat_flex <- simsurv(
   x = cov,
   knots = mod_flex$knots,
   logcumhazard = logcumhaz_flex,
-  maxt = 2500,
-  interval = c(1E-8, 2501)
+  maxt = 3000,
+  interval = c(1E-8, 3001)
 )
 
 # Merge covariate data and event times
 dat_weib <- merge(cov, dat_weib)
+# dat_weib$eventtime <- as.numeric(dat_weib$eventtime)
+# dat_weib$status <- as.numeric(dat_weib$status)
+# dat_weib$trt <- as.numeric(dat_weib$trt)
 dat_flex <- merge(cov, dat_flex)
+# dat_flex$eventtime <- as.numeric(dat_flex$eventtime)
+# dat_flex$status <- as.numeric(dat_flex$status)
+# dat_flex$trt <- as.numeric(dat_flex$trt)
 
 #%%
 
-fit_weib <- survfit(Surv(eventtime, status) ~ trt, data = dat_weib)
-fit_flex <- survfit(Surv(eventtime, status) ~ trt, data = dat_flex)
+fit_weib <- survfit(Surv(time = eventtime, event = status) ~ trt, data = dat_weib)
+fit_flex <- survfit(Surv(time = eventtime, event = status) ~ trt, data = dat_flex)
 ggsurvplot(
   fit_weib,
   data = dat_weib,
   size = 1,                 # change line size
   conf.int = TRUE,          # Add confidence interval
-  censor.shape="|",
+  censor.shape = "|",
   censor.size = 4,
+  mark = 1,
+  mark.time = dat_weib$eventtime[dat_weib$status == 1],
   pval = TRUE,              # Add p-value
   risk.table = TRUE,        # Add risk table
   legend.labs =
@@ -160,15 +196,15 @@ true_mod <- mod_flex
 # Define a function for conducting the simulation study
 sim_run <- function(true_mod) {
   # Covariate data
-  cov <- data.frame(id = 1:1000, trt = rbinom(1000, 1, 0.5))
+  cov <- data.frame(id = 1:ceiling(SSE$n), trt = rbinom(ceiling(SSE$n), 1, 0.5))
   # Simulate event times using simsurv
   dat <- simsurv(
     betas = true_mod$coefficients,
     x = cov,
     knots = true_mod$knots,
-    logcumhazard = logcumhaz,
-    maxt = 2500,
-    interval = c(1E-8, 2501)
+    logcumhazard = logcumhaz_flex,
+    maxt = 3000,
+    interval = c(1E-8, 3001)
   )
 
   # Merge covariate data and event times
@@ -197,8 +233,10 @@ sim_run <- function(true_mod) {
 }
 
 # Set seed for reproducibility
-set.seed(543543)
+set.seed(4020)
 
-# Run 100 replicates in simulation study and report the
+# Run 1000 replicates in simulation study and report the
 # estimated mean bias under the Weibull and FPM models
-out <- rowMeans(replicate(20, sim_run(true_mod = true_mod)))
+out <- rowMeans(replicate(1000, sim_run(true_mod = true_mod)))
+#  true_loghr    weib_bias    flex_bias
+#-0.345150476 -0.027285289 -0.003014806
